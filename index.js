507818699635dd1c3bd4542040216925ac5eccaf
MuriloAlvesSilva <mf.sniper@gmail.com>
2022-01-18 17:58:11 -0300

    Ajuste para rejeitar callback

--- index.js
+++ index.js
@@ -912,6 +912,40 @@ app.get('/confirmarOperacao',
 }
 });
 
+app.get('/rejeitarOperacao', 
+(req, resp)=> {
+
+       if(hasAuthorization(req)){
+              regLog("- Params ("+JSON.stringify(req.params)+")");
+              regLog("- confirmarOperacao -------------------------");
+              const res_data = req.body;
+              let retorno=" ";
+              let dt = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+              try {
+                     regLog(" Data "+dt)
+                     regLog(JSON.stringify(res_data))
+                     retorno = {
+                            "retornoConfirmacao": 1
+                     }
+
+             
+              }catch( e){
+              	regLog(e);
+              	
+                     retorno = {
+                            "retornoConfirmacao": 1
+                     }
+
+              }
+       regLog('--------------------------------------');
+       regLog(JSON.stringify(retorno))
+       resp.status(200).send(retorno);
+       regLog('--------------------------------------');
+}else{
+      semAutorizacao(req, resp);
+}
+});
+
 
 
 chamarDetalhes=( protocolo , hasMsg )=>{
